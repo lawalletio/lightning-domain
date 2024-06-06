e09159e6b5eaf1e2a6cf81bbb2f0d9111b6ec8dd
ferpa <feririarte.1996@gmail.com>
2024-06-06 14:08:55 -0300

    export federationConfig from .json

--- src/app/api/federation/route.ts
+++ src/app/api/federation/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { federationConfig } from '~/lib/federation';
+import federationConfig from '~/constants/federationConfig.json';
 
 export async function GET() {
   return NextResponse.json(federationConfig, { status: 200 });
