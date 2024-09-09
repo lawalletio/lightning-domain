1ed1b439391ad94d6f56ada663e93c13c1b377d3
Jona <llamasjonat@gmail.com>
2024-09-09 16:27:56 -0300

    fix(page): new structure for home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,70 @@
 import Link from 'next/link';
-import LaCryptaLogo from '~/components/LaCryptaLogo';
-import Logo from '~/components/Logo';
+import Image from 'next/image';
+import { TwitterLogoIcon, DiscordLogoIcon, GitHubLogoIcon } from '@radix-ui/react-icons';
+
+import { Button } from '~/components/ui/button';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#d2d2d2] to-[#fefefe]">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-black sm:text-[5rem]">
-          <Logo />
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-black hover:bg-white/20"
-            href="https://lacrypta.ar"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">
-              <LaCryptaLogo />
-            </h3>
-            <div className="text-lg">Conocé quiénes están detrás de todo esto.</div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-10 rounded-xl bg-white/10 p-4 pt-8 text-black hover:bg-white/20"
-            href="https://lawallet.io"
-            target="_blank"
-          >
-            <h3 className="text-3xl font-bold">Desarrolladores</h3>
-            <div className="text-lg">Verificá la documentación, es todo open source.</div>
-          </Link>
-        </div>
+    <div className="min-h-screen bg-background text-white flex flex-col items-center justify-center p-4">
+      <div className="max-w-4xl w-full space-y-12">
+        <Image className="mx-auto" src="/logotipo.svg" alt="LaWallet.io logo" width={165} height={30} />
+
+        <main className="space-y-12">
+          <section className="text-center max-w-2xl mx-auto">
+            <h1 className="text-3xl font-semibold mb-6">Todas tus herramientas en un solo lugar</h1>
+            <p className="mb-8 text-muted-foreground">
+              Gestioná pagos, identidades y comunidad fácilmente con LaWallet.app. Con la ayuda de Bitcoin, Lightning
+              Network y Nostr en una plataforma diseñada para simplificarlo todo.
+            </p>
+            <div className="flex flex-col sm:flex-row justify-center gap-4">
+              <Button
+                asChild
+                size="lg"
+                variant="outline"
+                className="bg-background text-foreground hover:bg-accent hover:text-accent-foreground text-lg px-8 py-6"
+              >
+                <Link href="https://app.lawallet.ar/login">Ingresá a tu billetera</Link>
+              </Button>
+              <Button
+                asChild
+                size="lg"
+                className="bg-primary text-primary-foreground hover:bg-primary/90 text-lg px-8 py-6"
+              >
+                <Link href="https://app.lawallet.ar/signup">Creá una ahora</Link>
+              </Button>
+            </div>
+          </section>
+        </main>
+
+        <footer className="text-center text-muted-foreground">
+          <div className="flex justify-center gap-1 mb-4">
+            <Button size="icon" variant="link" asChild>
+              <Link title="X/Twitter" href="https://twitter.com/lawalletok" target="_blank" rel="noopener noreferrer">
+                <TwitterLogoIcon />
+              </Link>
+            </Button>
+            <Button size="icon" variant="link" asChild>
+              <Link title="Github" href="https://github.com/lawalletio" target="_blank" rel="noopener noreferrer">
+                <GitHubLogoIcon />
+              </Link>
+            </Button>
+            <Button size="icon" variant="link" asChild>
+              <Link title="Discord" href="https://discord.gg/lawallet" target="_blank" rel="noopener noreferrer">
+                <DiscordLogoIcon />
+              </Link>
+            </Button>
+          </div>
+          <div className="flex justify-center items-center">
+            <p>&copy; 2024</p>
+            <Button asChild variant="link">
+              <Link href="https://lawallet.io/" title="LaWallet Labs">
+                lawallet.io
+              </Link>
+            </Button>
+          </div>
+        </footer>
       </div>
-    </main>
+    </div>
   );
 }
